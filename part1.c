fe4cbabe3380610b2c650e24e33658e9e9394792
Ayush_Mudgal <cs61c-cj@s277-18.CS.Berkeley.EDU>
2013-10-23 17:20:38 -0700

    proj3

--- part1.c
+++ part1.c
@@ -4,20 +4,33 @@
 int conv2D(float* in, float* out, int data_size_X, int data_size_Y,
                     float* kernel)
 {
+    
+    float newker[16];
+    int count2 = -1; 
+    for (int count = 0; count < 16; count++) {
+      if (count <= 3 || count % 4 == 0) {
+	    newker[count] = 0;
+      }
+      else {
+	count2++;
+	newker[count] = kernel[count2];
+      }
+    }
+    kernel = newker;
     // the x coordinate of the kernel's center
     int kern_cent_X = (KERNX - 1)/2;
     // the y coordinate of the kernel's center
     int kern_cent_Y = (KERNY - 1)/2;
     
     // main convolution loop
-	for(int x = 0; x < data_size_X; x++){ // the x coordinate of the output location we're focusing on
-		for(int y = 0; y < data_size_Y; y++){ // the y coordinate of theoutput location we're focusing on
-			for(int i = -kern_cent_X; i <= kern_cent_X; i++){ // kernel unflipped x coordinate
+	for(int y = 0; y < data_size_Y; y++){ // the y coordinate of theoutput location we're focusing on
+		for(int x = 0; x < data_size_X; x++){ // the x coordinate of the output location we're focusing on
 				for(int j = -kern_cent_Y; j <= kern_cent_Y; j++){ // kernel unflipped y coordinate
+					for(int i = -kern_cent_X; i <= kern_cent_X; i++){ // kernel unflipped x coordinate
 					// only do the operation if not out of bounds
-					if(x+i>-1 && x+i<data_size_X && y+j>-1 && y+j<data_size_Y){
+						if(x+i>-1 && x+i<data_size_X && y+j>-1 && y+j<data_size_Y){
 						//Note that the kernel is flipped
-						out[x+y*data_size_X] += 
+							out[x+y*data_size_X] += 
 								kernel[(kern_cent_X-i)+(kern_cent_Y-j)*KERNX] * in[(x+i) + (y+j)*data_size_X];
 					}
 				}

b4b87ad04229974a30fb11069b1ab47b1344d835
Ayush_Mudgal <cs61c-cj@hive12.CS.Berkeley.EDU>
2013-10-24 02:24:14 -0700

    revised version; back to what it was

--- part1.c
+++ part1.c
@@ -1,21 +1,21 @@
 #include <emmintrin.h>
-#define KERNX 3 //this is the x-size of the kernel. It will always be odd.
+#define KERNX 3//this is the x-size of the kernel. It will always be odd.
 #define KERNY 3 //this is the y-size of the kernel. It will always be odd.
 int conv2D(float* in, float* out, int data_size_X, int data_size_Y,
                     float* kernel)
 {
-  //printf("old \n");
-  /*for (int i = 0; i < 9; i++) {
+  printf("old \n"); //print loop
+  for (int i = 0; i < 9; i++) {
 	  if (i % 3 == 0) {
 	    printf("\n");
 	  }
 	  printf(" %f ", kernel[i]);
-	}*/
+	}
 	
-    float newker[12];
+    float newker[12]; // kerner conversion (padding to the left)
     int count2 = -1; 
     for (int count = 0; count < 12; count++) {
-      if (count % 4 == 0) {
+      if ((count) % 4 == 0) {
 	    newker[count] = 0;
       }
       else {
@@ -23,16 +23,17 @@ int conv2D(float* in, float* out, int data_size_X, int data_size_Y,
 	newker[count] = kernel[count2];
       }
     }
-    printf("\n new");
+    kernel = newker;
+
+    printf("\n new"); //print loop; new kernel
     for (int i = 0; i < 12; i++) {
 	  if (i % 4 == 0) {
 	    printf("\n");
 	  }
 	  printf(" %f ", newker[i]);
 	}
-    kernel = newker;
     
-    data_size_X += 3;
+    data_size_X += 3; //padding the input matrix all around
     data_size_Y += 2;
     int size = (data_size_X) * (data_size_Y);
     float newin[size];
@@ -47,23 +48,23 @@ int conv2D(float* in, float* out, int data_size_X, int data_size_Y,
       }
     }
     in = newin;
- 
     // the x coordinate of the kernel's center
     int kern_cent_X = (KERNX - 1)/2;
     // the y coordinate of the kernel's center
     int kern_cent_Y = (KERNY - 1)/2;
-    
+    //printf("center of the kernel %d %d", kern_cent_X, kern_cent_Y); //uncomment to print the center of the kernel
+   
     // main convolution loop
 	for(int y = 0; y < data_size_Y; y++){ // the y coordinate of theoutput location we're focusing on
 		for(int x = 0; x < data_size_X; x++){ // the x coordinate of the output location we're focusing on
 				for(int j = -kern_cent_Y; j <= kern_cent_Y; j++){ // kernel unflipped y coordinate
 					for(int i = -kern_cent_X; i <= kern_cent_X; i++){ // kernel unflipped x coordinate
 					// only do the operation if not out of bounds
-					//if(x+i>-1 && x+i<data_size_X && y+j>-1 && y+j<data_size_Y){
+					if(x+i>-1 && x+i<data_size_X && y+j>-1 && y+j<data_size_Y){
 						//Note that the kernel is flipped
 							out[x+y*data_size_X] += 
 								kernel[(kern_cent_X-i)+(kern_cent_Y-j)*KERNX] * in[(x+i) + (y+j)*data_size_X];							
-					//}
+					}
 				}
 			}
 		}
